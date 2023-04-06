bc3de94dd30ba021d137bbe52d2689ad42dd75e8
Ankita Tiwari <ankitatiwari12001@gmail.com>
2023-04-06 18:07:01 +0530

    first-commit

---
+++ keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoUrl: "mongodb+srv://ankitatiwari12001:Ankitatiwari005@cluster0.fnf8atm.mongodb.net/mernstack?retryWrites=true&w=majority",
+    Jwt_secret: "faslkfocvneofu"
+
+};
